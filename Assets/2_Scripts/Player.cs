8855fb51926b2ec73d0cc82e6fb32f87c48acd6e
baleunbit <vahyeon2514@sonline20.sen.go.kr>
2025-07-17 13:21:10 +0900

    change

--- Assets/2_Scripts/Player.cs
+++ Assets/2_Scripts/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 
-public class CharacterController : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [Header("이동 설정")]
     public float moveSpeed = 5f;
