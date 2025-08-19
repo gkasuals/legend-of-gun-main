75e3aea06d393d584e9dfa6287ff77daaf1079d0
baleunbit <vahyeon2514@sonline20.sen.go.kr>
2025-08-19 15:03:58 +0900

    health bar

--- Assets/2_Scripts/FollowPlayer.cs
+++ Assets/2_Scripts/FollowPlayer.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class FollowPlayerYPlusOne : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     public Transform playerTransform; // 플레이어의 Transform을 에디터에서 할당
 
@@ -11,7 +11,7 @@ public class FollowPlayerYPlusOne : MonoBehaviour
             // 현재 오브젝트의 위치를 플레이어의 위치로 따라가되, Y축만 +1
             Vector3 targetPosition = new Vector3(
                 playerTransform.position.x,
-                playerTransform.position.y + 1f,
+                playerTransform.position.y + 0.8f,
                 playerTransform.position.z
             );
 
